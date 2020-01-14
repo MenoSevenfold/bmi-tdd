8c5b096a9d9b0ff59212fa2e4597baa1e56f33a3
ShakedBen <shakedhamo@gmail.com>
2020-01-14 12:12:38 +0200

    Init Project with files

---
+++ bmi.py
@@ -0,0 +1,32 @@
+def calculate_bmi(height,weight):
+    if not(height.isnumeric()) or not(weight.isnumeric()):
+        return
+    elif height=='0':
+        return
+    elif int(height)<0 or int(weight)<0:
+        return
+
+    BMI = int(weight) / (int(height) * int(height))
+    return int(BMI)
+
+def bmi_check(BMI):
+    if BMI==None:
+        return 'wrong input'
+    elif BMI<18.5:
+        return 'Underweight'
+    elif 18.5<BMI<24.9:
+        return 'OK'
+    elif 25<BMI<29.9:
+        return 'overweight'
+    elif 30<BMI<34.9:
+        return 'obesity'
+    elif 35<BMI:
+        return 'Severe obesity'
+
+
+weight=input("enter your weight")
+height=input("enter your height")
+BMI=calculate_bmi(height,weight)
+print(bmi_check(BMI))
+#
+
